e74dd08444bcfef67fd3058a15edac8e457c0eb9
quoctuan06122009 <quoctuan06122009@gmail.com>
2012-06-18 13:40:16 +0000

    moi moi

---
+++ MvcStationeryManagementSystem/MvcStationeryManagementSystem/Controllers/Default1Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcStationeryManagementSystem.Controllers
+{
+    public class Default1Controller : Controller
+    {
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
